3670ad2ce7eccade9d8e63d0bedac06df85c9062
hkumar <hkumar@accuratebackground.com>
2019-08-28 22:37:47 +0530

    hero-component for-loop edit

--- angular8-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular8-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { Heroes } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,13 +8,18 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero:Hero = {
+  selectedHero: Hero;
+  heroes = Heroes;
+  hero: Hero = {
     id: 1,
-    name: 'Windstorm'
-  };
+    name: "windstorm"
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
