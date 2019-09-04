3046c4c8890560468ccd930d1768633829e8a689
hkumar <hkumar@accuratebackground.com>
2019-09-04 23:53:42 +0530

    Services sync and Async

--- angular8-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular8-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { Heroes } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,17 @@ import { Heroes } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = Heroes;
-  hero: Hero = {
-    id: 1,
-    name: "windstorm"
-  }
-  constructor() { }
+  heroes = Hero[];
+  
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(Heroes => this.heroes = Heroes);
+  }
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
